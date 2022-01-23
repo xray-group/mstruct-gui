07aaf358ebe21d7b7e980072713c00990c5247e4
Kuba <Kuba@barbucha>
2022-01-23 15:50:06 +0100

    Reorder UVW to WVU on pVoight profiles

--- src/main/java/cz/kfkl/mstruct/gui/model/ReflectionProfilePVoigtAElement.java
+++ src/main/java/cz/kfkl/mstruct/gui/model/ReflectionProfilePVoigtAElement.java
@@ -12,15 +12,17 @@ public class ReflectionProfilePVoigtAElement extends ReflectionProfileModel<Refl
 	private static final String FXML_FILE_NAME = "reflectionProfilePVoigtA.fxml";
 
 	@XmlUniqueElement
-	public ParUniqueElement uPar = new ParUniqueElement("U");
+	public ParUniqueElement wPar = new ParUniqueElement("W");
 	@XmlUniqueElement
 	public ParUniqueElement vPar = new ParUniqueElement("V");
 	@XmlUniqueElement
-	public ParUniqueElement wPar = new ParUniqueElement("W");
+	public ParUniqueElement uPar = new ParUniqueElement("U");
+
 	@XmlUniqueElement
 	public ParUniqueElement eta0Par = new ParUniqueElement("Eta0");
 	@XmlUniqueElement
 	public ParUniqueElement eta1Par = new ParUniqueElement("Eta1");
+
 	@XmlUniqueElement
 	public ParUniqueElement asym0Par = new ParUniqueElement("Asym0");
 	@XmlUniqueElement
@@ -40,7 +42,7 @@ public class ReflectionProfilePVoigtAElement extends ReflectionProfileModel<Refl
 
 	@Override
 	public List<ParUniqueElement> getParams() {
-		return List.of(uPar, vPar, wPar, eta0Par, eta1Par, asym0Par, asym1Par, asym2Par);
+		return List.of(wPar, vPar, uPar, eta0Par, eta1Par, asym0Par, asym1Par, asym2Par);
 	}
 
 }
